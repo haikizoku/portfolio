34214d25f290588f7e57e6c6a744c77b8f652ccd
Gregory <gregory.norvene@segeco.fr>
2020-03-18 23:34:54 +0100

    style : remove unused link file

--- header.php
+++ header.php
@@ -11,9 +11,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="dist/sweetalert.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="dist/sweetalert.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="animate.min.css">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 
