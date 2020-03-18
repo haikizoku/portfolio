d5f9cc8852e5bd3351bbfc4b64bad087f6e42710
Gregory <gregory.norvene@segeco.fr>
2020-03-18 21:12:04 +0100

    feat : add link gitlab

--- footer.php
+++ footer.php
@@ -8,6 +8,7 @@
             <div class="navbar-text pull-right">
                 <div class="socialNetwork">
                     <span><a href="https://github.com/haikizoku" class="tips" target="_blank" title="follow me on github"><i class="fa fa-github fa-2x" style="color:#149C4D;"></i></a></span>
+                    <span><a href="https://gitlab.com/react25" class="tips" target="_blank" title="follow me on gitlab"><i class="fa fa-gitlab fa-2x" style="color:#FFA500;"></i></a></span>
                     <span><a href="http://fr.viadeo.com/fr/profile/gregory.norvene" target="_blank" class="tips" title="follow me on viadeo"><i class="fa fa-viadeo-square fa-2x"></i></a></span>
                     <span><a href="https://www.linkedin.com/in/gregory-norvene-ab6519124" target="_blank" class="tips" title="follow me on linkdelin"><i class="fa fa-linkedin-square fa-2x" style="color:#007bff;"></i></a></span>
                 </div>
